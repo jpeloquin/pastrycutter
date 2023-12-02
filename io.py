d1efc9d05202d54e9dfb313fdbfc4c3f6c78ad05
John Peloquin <john.peloquin@gmail.com>
2023-12-01 21:03:35 -0500

    Fix extra negative sign

--- io.py
+++ io.py
@@ -53,7 +53,7 @@ def read_affine_mat_3d(pth: Union[str, Path]):
     a = ants_affine["AffineTransform_double_3_3"]
     # fmt: off
     affine = np.array([
-         [a[0][0], -a[1][0], -a[2][0], -a[-3][0]],
+         [a[0][0], a[1][0], -a[2][0], -a[-3][0]],
          [a[3][0], a[4][0], -a[5][0], -a[-2][0]],
          [-a[6][0], -a[7][0], a[8][0], a[-1][0]],
          [0, 0, 0, 1],

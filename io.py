Fix extra negative sign

io.py
@@ -53,7 +53,7 @@ def read_affine_mat_3d(pth: Union[str, Path]):
     a = ants_affine["AffineTransform_double_3_3"]
     # fmt: off
     affine = np.array([
-         [a[0][0], -a[1][0], -a[2][0], -a[-3][0]],
+         [a[0][0], a[1][0], -a[2][0], -a[-3][0]],
          [a[3][0], a[4][0], -a[5][0], -a[-2][0]],
          [-a[6][0], -a[7][0], a[8][0], a[-1][0]],
          [0, 0, 0, 1],